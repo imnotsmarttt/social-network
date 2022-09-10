2ece7bad7086e84e1cd65fd9e17f66b0bfe9cd00
imnotsmarttt <imnotsmarttt@gmail.com>
2022-09-10 13:27:23 +0300

    Text search

--- search/views.py
+++ search/views.py
@@ -1,5 +1,6 @@
+from django.db.models import Q
 from django.views.generic import ListView
-from django.contrib.postgres.search import SearchVector, TrigramWordSimilarity
+from django.contrib.postgres.search import SearchVector, TrigramWordSimilarity, TrigramSimilarity
 from users.models import CustomUser
 
 
@@ -21,7 +22,6 @@ class SearchUsers(ListView):
         search_input = self.request.GET.get('search-input')
 
         if is_valid_queryset_param_stroke(search_input):
-            # qs = qs.annotate(similarity=TrigramWordSimilarity(search_input, 'username'),).filter(similarity__gt=0.3).order_by('-similarity')
-            qs = qs.annotate(search=SearchVector('username', 'first_name', 'last_name')).filter(search=search_input)
-        print(qs, search_input)
+            qs = qs.annotate(similarity=TrigramWordSimilarity(search_input, 'username'),).filter(similarity__gt=0.3).order_by('-similarity')
+
         return qs
