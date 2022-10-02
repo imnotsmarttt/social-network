402d09bb88f694c0060a551e4e85665ba42d291d
imnotsmarttt <imnotsmarttt@gmail.com>
2022-10-02 19:28:10 +0300

    Fix bugs

--- contacts/views.py
+++ contacts/views.py
@@ -12,11 +12,12 @@ def add_contact(request):
     action = request.POST.get('action')
     user_to = request.POST.get('id')
     if action and user_to:
-
         user = CustomUser.objects.get(id=user_to)
         if action == 'follow':
+            print(1)
             Contact.objects.get_or_create(user_from=request.user, user_to=user)
         else:
+            print(2)
             Contact.objects.filter(user_from=request.user, user_to=user).delete()
 
     return JsonResponse({'status':'ok'})
--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
-from django.db.models import Q
 from django.views.generic import ListView
 from django.contrib.postgres.search import TrigramWordSimilarity
+from django.db.models import Q
+
 from users.models import CustomUser
 
 
@@ -13,6 +14,7 @@ def is_valid_queryset_param_stroke(param):
 
 
 class SearchUsers(ListView):
+    """Search users"""
     model = CustomUser
     template_name = 'search/search.html'
     context_object_name = 'users'
@@ -22,7 +24,16 @@ class SearchUsers(ListView):
         search_input = self.request.GET.get('search-input')
 
         if is_valid_queryset_param_stroke(search_input):
-            qs = qs.annotate(similarity=TrigramWordSimilarity(search_input, 'username'),).filter(similarity__gt=0.3)
+            # Search user by first name, last name, username
+            qs = qs.annotate(
+                similarity_username=TrigramWordSimilarity(search_input, 'username'),
+                similarity_first=TrigramWordSimilarity(search_input, 'first_name'),
+                similarity_last=TrigramWordSimilarity(search_input, 'last_name'),
+            ).filter(
+                Q(similarity_username__gt=0.3) |
+                Q(similarity_first__gt=0.3) |
+                Q(similarity_last__gt=0.3)
+            )
 
         return qs
 
